608620681390f843f251c87bff77eda078155afe
Sougnabe <s.payang@alustudent.com>
2024-10-25 21:26:56 +0000

    ok

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     """documentation"""
     reddit_url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
-
+"""try it """
     try:
         response = requests.get(reddit_url, headers=headers, allow_redirects=False)
 
