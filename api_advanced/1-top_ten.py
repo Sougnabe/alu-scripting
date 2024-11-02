93a75ce1a2273e0088f9eaf4c29b5ebf3917472b
Sougnabe <s.payang@alustudent.com>
2024-11-02 10:52:01 +0000

    ok

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,8 +5,16 @@
 import sys
 
 if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
+    try:
+        # Import the top_ten function from 1-top_ten
+        top_ten = __import__('1-top_ten').top_ten
+    except AttributeError:
+        print("Error: '1-top_ten' module does not contain 'top_ten' function.")
+        sys.exit(1)
+
+    # Check if an argument is provided
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
+        # Call the top_ten function with the subreddit argument
         top_ten(sys.argv[1])
