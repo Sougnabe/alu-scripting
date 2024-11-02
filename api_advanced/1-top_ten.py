d675ef3ea977d28570f7a69770da96bb6443b817
Sougnabe <s.payang@alustudent.com>
2024-11-02 10:46:38 +0000

    ok

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
-"""Importation."""
-import requests
+"""
+1-main
+"""
+import sys
 
-def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'python:subreddit.top.ten:v1.0 (by /u/yourusername)'}
-    
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        data = response.json().get('data', {}).get('children', [])
-        if data:
-            for post in data:
-                print(post['data']['title'])
-        else:
-            print(None)
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
     else:
-        print(None)
+        top_ten(sys.argv[1])
